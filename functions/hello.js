c79a8b48fd17ed2333766eb28313b9f4fee1b076
J0Var4lM <49831808+w754248A3@users.noreply.github.com>
2025-10-08 20:23:27 +0800

    测试查询

--- functions/hello.js
+++ functions/hello.js
@@ -1,3 +1,13 @@
-export function onRequest(context) {
-  return new Response("Hello, world!");
+export async function onRequest(context) {
+    try{
+        // Create a prepared statement with our query
+        const ps = context.env.database_name.prepare("SELECT * FROM images");
+        const data = await ps.first();
+
+        return Response.json(data);
+    }
+    catch(e){
+        return Response.json(e);
+    }
+  
 }
