b76f781acbc0b774d3ad4abaf9aa5b6ae0db9f6e
J0Var4lM <49831808+w754248A3@users.noreply.github.com>
2026-01-15 12:17:54 +0800

    只显示测试页面

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+//import './style.css'
+//import App from './App.vue'
+import FileList from './pages/FileList.vue';
 //import FileShared from './pages/FileShared.vue'
 
 
-createApp(App).mount('#app');
+createApp(FileList).mount('#app');
